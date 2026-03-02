93ea0f95bc48f54edc6c403a9a2dd932803afceb
artem <artemgolovachov1256789@gmail.com>
2026-03-02 19:13:30 +0100

    data validation check

--- 2_DW_Mart_build/02_load_schema_dq.sql
+++ 2_DW_Mart_build/02_load_schema_dq.sql
@@ -26,3 +26,31 @@ FROM read_csv(
 INSERT INTO skills_job_dim (skill_id, job_id)
 SELECT skill_id, job_id
 FROM read_csv('https://storage.googleapis.com/sql_de/skills_job_dim.csv', auto_detect=true);
+
+
+
+select 'Company Dim' as table_name, count(*) as record_count  from company_dim
+union all
+select 'Skills Dim', count(*) from skills_dim
+union all
+select 'Job Postings Fact', count(*) from job_postings_fact
+union all
+select 'Skills Job Dim', count(*) from skills_job_dim;
+
+select * from company_dim limit 5;
+
+
+
+
+
+SELECT '=== Company Dimension Sample ===' AS info;
+SELECT * FROM company_dim LIMIT 5;
+
+SELECT '=== Skills Dimension Sample ===' AS info;
+SELECT * FROM skills_dim LIMIT 5;
+
+SELECT '=== Job Postings Fact Sample ===' AS info;
+SELECT * FROM job_postings_fact LIMIT 5;
+
+SELECT '=== Skills Job Bridge Sample ===' AS info;
+SELECT * FROM skills_job_dim LIMIT 5;
