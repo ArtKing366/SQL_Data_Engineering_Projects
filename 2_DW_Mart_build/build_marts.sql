b3b45ab4f177959b66f556b905a8013b0d37edb1
artem <artemgolovachov1256789@gmail.com>
2026-03-03 20:41:15 +0100

    build priority mart, and update mart

--- 2_DW_Mart_build/build_marts.sql
+++ 2_DW_Mart_build/build_marts.sql
@@ -1,6 +1,16 @@
+-- duckdb dw_marts.duckdb -c ".read build_marts.sql"
+
 .read 01_create_tables_dw.sql
 
 .read 02_load_schema_dq.sql
 
 .read 03_create_flat_mart.sql
+
+.read 04_create_skills_mart.sql
+
+.read 05_create_priority_mart.sql
+
+.read 06_update_priority_mart.sql
+
+
 
